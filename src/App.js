5440bd21d3709c8db59863527b5fd27c351a3df8
pavaniiiii <pavankumaryaragogu116@gmail.com>
2023-01-24 12:23:51 +0530

    Create a React app and publish it to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -2,24 +2,34 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const html_code=document.querySelector(".html-code textarea");
+  const css_code=document.querySelector(".css-code textarea");
+  const js_code=document.querySelector(".js-code textarea");
+  const result=document.querySelector("#result")
+  html_code.value=localStorage.html_code;
+  css_code.value=localStorage.css_code;
+  js_code.value=localStorage.js_code;
+
+  function run (){
+    localStorage.setItem("html_code",html_code.value);
+    localStorage.setItem("css_code",css_code.value);
+    localStorage.setItem("js_code",js_code.value)
+
+    result.contentDocument.body.innerHTML= `<style>${localStorage.css_code}</style>` + localStorage.html_code
+    result.contentWindow.eval(localStorage.js_code)
+
+}
+html_code.onkeyup = () =>run(); 
+css_code.onkeyup = () =>run(); 
+js_code.onkeyup = () =>run(); 
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
 }
 
 export default App;
+
